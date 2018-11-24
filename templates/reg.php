3f81d5ce450f94af637ef71cc858efa6b083f060
alenka777555 <39382087+alenka777555@users.noreply.github.com>
2018-11-24 11:06:26 +0300

    Add files via upload

---
+++ templates/reg.php
@@ -0,0 +1,7 @@
+
+
+<form method="post" action="http://site7/engine/module/reg.php">
+    <input name="username">
+    <input name="password" type="password">
+    <input type="submit" value="Отправить данные">
+</form>
