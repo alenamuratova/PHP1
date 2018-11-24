05459e117454a30b17302f5b023c0bfcf51fcadf
alenka777555 <39382087+alenka777555@users.noreply.github.com>
2018-11-24 11:01:59 +0300

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+define('SITE_ROOT', '../');
+define('WWW_ROOT', SITE_ROOT . 'public');
+
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '12345678');
+define('DB', 'lesson7');
+
+define('DATA_DIR', SITE_ROOT . 'data');
+define('FILES_DIR', SITE_ROOT. 'files/');
+define('VAR_DIR', SITE_ROOT . 'var/');
+define('LIB_DIR', SITE_ROOT . 'engine/');
+define('TPL_DIR', SITE_ROOT . 'templates/');
+
+define('SITE_TITLE', 'Урок 7');
+
